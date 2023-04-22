fb6a5699725d6f659aed669e677ba21c1b8dd1f9
Eduardo Oliveira <67022479+Edullk@users.noreply.github.com>
2023-04-22 17:12:31 -0300

    Delete Estrutura de dados directory

--- Estrutura de dados/conjunto.py
+++
@@ -1,9 +0,0 @@
-conjunto_cores = {"vermelho", "azul", "verde"}
-
-# Não deixa adicionar indices repetidos
-
-conjunto_cores.add("branco")
-conjunto_cores.add("vermelho")
-conjunto_cores.remove("branco")
-
-print(conjunto_cores)
